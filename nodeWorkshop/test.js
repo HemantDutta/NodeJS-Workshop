fce3ca3fa23cf1dc65e399c2cbc0c54801900235
HemantDutta <hemantnorddutta2@gmail.com>
2022-12-19 20:39:52 +0530

    routing fixed

--- nodeWorkshop/test.js
+++ nodeWorkshop/test.js
@@ -1,6 +1,6 @@
 let http = require('http'); //Importing http package
 let port = 3000; //Defining port number
-let host = '10.4.219.193' //my ip
+let host = 'localhost' //my ip
 
 //install npm i fs and npm i express after going home
 let fs = require('fs');
@@ -11,7 +11,7 @@ let server = http.createServer(function(req,res){
     // res.end("<h1 style='color:green'>Hemant Dutta</h1>")
     let url = req.url;
     if(url=="/"){
-        fs.readFile(path.join(__dirname,'index.html'),'utf-8',(err,data)=>{
+        fs.readFile('index.html','utf-8',(err,data)=>{
             if(err) throw err;
             res.end(data);
         });
