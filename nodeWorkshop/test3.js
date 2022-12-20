34e30ac20f04cd19bd8201ac9adbf5f541ee919d
HemantDutta <hemantnorddutta2@gmail.com>
2022-12-20 12:21:19 +0530

    routing fix

--- nodeWorkshop/test3.js
+++ nodeWorkshop/test3.js
@@ -3,7 +3,11 @@ const {load} = require('./test1.js');
 const express = require('express');
 const app = express();
 const port = 3003;
+// const cors = require('cors');
+// app.use(cors({
+//     origin:"*",
+// }));
 app.listen(port, ()=>console.log(`Server is waiting at ${port}`));
 app.get('/user', (req,res)=>{
-    res.send(load);
+    res.send(load());
 })
